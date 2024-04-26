5dc246cde4d8a7566e13c6c46089aaa55c0ca4ec
artyg <artjoms.grisajevs@student.nhlstenden.com>
2024-04-26 18:10:10 +0200

    Add: Objects

--- main.go
+++ main.go
@@ -6,9 +6,34 @@ import (
 	"net/http"
 )
 
+type Teacher struct {
+	Id               string `json:"id"`
+	FirstName        string `json:"first_name"`
+	LastName         string `json:"last_name"`
+	Email            string `json:"email"`
+	Password         string `json:"password"`
+	RegistrationDate string `json:"registration_date"`
+}
+
+type Student struct {
+	Id        string `json:"id"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+}
+
+type Group struct {
+	Id string `json:"id"`
+}
+
+type Subject struct {
+	Id      int     `json:"id"`
+	Name    string  `json:"name"`
+	Teacher Teacher `json:"teacher"`
+}
+
 func main() {
-	http.HandleFunc("/", getRoot)
-	http.HandleFunc("/hello", getHello)
+	http.HandleFunc("/", getRoot)     // Website
+	http.HandleFunc("/api", getHello) //
 
 	err := http.ListenAndServe(":8080", nil)
 	panic(err.Error())
